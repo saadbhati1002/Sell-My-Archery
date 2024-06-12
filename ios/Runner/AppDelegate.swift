a0b6c366982921ef467e2ed1731921bfcea67423
saadbhati1002 <saadbhati1002@gmail.com>
2024-06-12 19:14:35 +0530

    feat: ios setup

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -60,7 +60,6 @@
 import UIKit
 import Flutter
 import Firebase
-import Braintree
 import GoogleMaps
 import google_mobile_ads
 
@@ -83,7 +82,6 @@ import google_mobile_ads
     FLTGoogleMobileAdsPlugin.registerNativeAdFactory(
         self, factoryId: "listTile", nativeAdFactory: listTileFactory)
         
-    BTAppContextSwitcher.setReturnURLScheme("com.sellmyarchary.chirag.braintree")
        
    if #available(iOS 10.0, *) {
      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
@@ -92,34 +90,5 @@ import google_mobile_ads
     return super.application(application, didFinishLaunchingWithOptions: launchOptions);
   }
   
-
-  override
-    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
-    if url.scheme?.localizedCaseInsensitiveCompare("com.sellmyarchary.chirag.braintree") == .orderedSame {
-        return BTAppContextSwitcher.handleOpenURL(url)
-    }
-    return false
-  }
-
-// If you support iOS 8, add the following method.
-override
-func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
-    if url.scheme?.localizedCaseInsensitiveCompare("com.sellmyarchary.chirag.braintree") == .orderedSame {
-        return BTAppContextSwitcher.handleOpenURL(url)
-    }
-    return false
-}
-  override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        let firebaseAuth = Auth.auth()
-        firebaseAuth.setAPNSToken(deviceToken, type: AuthAPNSTokenType.unknown)
-
-    }
-    override func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        let firebaseAuth = Auth.auth()
-        if (firebaseAuth.canHandleNotification(userInfo)){
-            print(userInfo)
-            return
-        }
-    }
 }
 //FLTGoogleMobileAdsPlugin
--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'dart:io';
 import 'package:camera/camera.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+// import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -62,7 +62,7 @@ Future<void> main() async {
     Utils.psPrint(e.toString());
   }
 
-  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  // FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
   // FirebaseMessaging.onBackgroundMessage(Utils.myBackgroundMessageHandler);
 
   // NativeAdmob(adUnitID: Utils.getAdAppId());
